a9b2400fa16388ba72bff88d9dad5a0f64064053
MHA <98241106+MHA963@users.noreply.github.com>
2024-04-29 11:37:51 +0200

    fixing traceparent &apigateway

--- Nozama/Nozama.apigateway/index.js
+++ Nozama/Nozama.apigateway/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const {setupCreditCheck} = require("./auth/creditcheck");
+const crypto = require('crypto')
 
 const {ROUTES} = require("./auth/routes");
 const {setupRateLimit} = require("./auth/ratelimit");
@@ -7,6 +8,7 @@ const {setupRateLimit} = require("./auth/ratelimit");
 const {setupLogging} = require("./auth/logging");
 const {setupProxies} = require("./auth/proxy");
 const {setupAuth} = require("./auth/auth");
+const TraceParent = require('traceparent')
 
 const app = express()
 const port = 3000;
@@ -14,11 +16,15 @@ const port = 3000;
 // Middleware to add traceparent header to all incoming requests
 app.use((req, res, next) => {
     // Generate traceparent header
-    const traceparent = TraceParent.fromRandom();
-    
-    // Add traceparent header to the request
-    req.headers['traceparent'] = traceparent.toString();
-    
+    const version = Buffer.alloc(1).toString('hex')
+    const traceId = crypto.randomBytes(16).toString('hex')
+    const id = crypto.randomBytes(8).toString('hex')
+    const flags = '01'
+
+    const header = `${version}-${traceId}-${id}-${flags}`
+
+    const parent = TraceParent.fromString(header)
+
     // Continue to the next middleware
     next();
 });
